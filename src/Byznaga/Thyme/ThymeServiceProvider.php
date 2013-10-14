dde8e2bccec3a9901caadce8557bc2c6d7fe1b96
Andrew Bliss <andrewgbliss@gmail.com>
2013-10-14 11:54:49 -0600

    init commit

---
+++ src/Byznaga/Thyme/ThymeServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Byznaga\Thyme;
+
+use Illuminate\Support\ServiceProvider;
+
+class ThymeServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
